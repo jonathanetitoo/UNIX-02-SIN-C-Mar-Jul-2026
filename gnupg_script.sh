8938f5464f31715f8a1df26edef11e2618bb22ee
martinfuel123-ai <martinfuel123@gmail.com>
2026-03-23 15:11:35 +0000

    Evidencia: importé y agregué la clave pública de mi compañero

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,11 @@
-#Este comando en lista la clave privada 
+# This command lists your private GPG keys
 gpg --list-secret-keys --keyid-format=long
-#Este es mi respaldo de mi clave privada GPG
+
+# This is my backup of my private GPG key
 gpg --armor --export-secret-keys EACB56901165A8B5 > private.key
+
+# Export my public key
+gpg --armor --export martinfuel123@gmail.com > jt_public_key.asc
+
+# Import my friend's public key
+gpg --import micompa-public-key.asc
