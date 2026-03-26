df27529c76b8ec685ba11b0a5946ae79347bb871
martinfuel123-ai <martinfuel123@gmail.com>
2026-03-26 21:09:18 +0000

    Add clearsign command for document

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -9,3 +9,27 @@ gpg --armor --export martinfuel123@gmail.com > jt_public_key.asc
 
 # Import my friend's public key
 gpg --import micompa-public-key.asc
+
+# Check the keys you have (public keys)
+gpg --list-keys
+
+# Send a message in a file (plain text)
+echo "message is secret hello Rafael, I am Jose Martin" > doc_unencrypted.txt
+
+# View the message
+cat doc_unencrypted.txt
+
+# Copy your friend's key ID or fingerprint
+gpg --list-keys
+
+# Encrypt the document for your friend using their key
+gpg --output doc_encrypted.txt --encrypt --recipient "Rafael Patin" doc_unencrypted.txt
+
+# View the encrypted document
+cat doc_encrypted.txt
+
+# Decrypt the message from my friend
+gpg --decrypt doc_encrypted_by_patin.txt
+
+## Sign file in clear text format
+gpg --clearsign doc_no_cifrado.txt
