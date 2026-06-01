4e13733717f53e128cd9673010d0690d66a2eaa5
josev2609 <jovelascoso@uide.edu.ec>
2026-06-01 15:19:19 +0000

    fc

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,4 +1,6 @@
-bash --version 
+#! /bin/bash 
+set -x 
+#bash --version 
 env 
 echo ${SHELL}
 echo ${RANDOM}
@@ -8,12 +10,13 @@ ps -e -f
 ps -ef
 df -human -readable
 #all environment variables must be in uppercase.
- bash -x blackhatbash1.sh
+ #bash -x blackhatbash1.sh
  #Give a lot of informotion 
-  bash -r blackhatbash1.sh
+  #bash -r blackhatbash1.sh
   #create a restricted bash shell
   chmod u+x blackhatbash1.sh 
   #change permissions 
-  ./blackhatbash1.sh 
+  #./blackhatbash1.sh 
   #execute the scrypt 
-  bash -n blackhatbash1.sh 
+  #bash -n blackhatbash1.sh 
+  set +x
