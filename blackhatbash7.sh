5c0833fdf5caded38c63aa588c9b62e14d793c29
josev2609 <jovelascoso@uide.edu.ec>
2026-06-19 15:14:09 +0000

    grep

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -1,2 +1,8 @@
 touch example_file1 example_file2 example_file3
 
+ grep "35.237.4.214" log.txt
+  grep "35.237.4.214\|13.66.139.0" log.txt
+  $ grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+  ps 
+  $ ps | grep -i tty
+  $ grep -v "35.237.4.214" log.txt
