571bdcb86c2018d767a8375b5a11d5a09b6c17f1
josev2609 <jovelascoso@uide.edu.ec>
2026-06-19 15:43:48 +0000

    log

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -19,4 +19,4 @@ touch example_file1 example_file2 example_file3
 grep "42.236.10.117" log.txt | awk '{print $7
 # together: they filter the log by that IP and then display only the value in column 7
  sed 's/Mozilla/Godzilla/g' log.txt
- 
+ $ sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
