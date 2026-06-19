da26bf24a47ac91c2286de0b4af1cdcf087d3b83
josev2609 <jovelascoso@uide.edu.ec>
2026-06-19 15:49:21 +0000

    sed

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -20,3 +20,9 @@ grep "42.236.10.117" log.txt | awk '{print $7
 # together: they filter the log by that IP and then display only the value in column 7
  sed 's/Mozilla/Godzilla/g' log.txt
  $ sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+  sed 's/ //g' log.txt
+  $ sed '1d' log.txt
+   sed '$d' log.txt
+   $ sed '5,7d' log.txt
+   $ sed -n '2,15 p' log.txt
+   $ sed -i '1d' log.txt
