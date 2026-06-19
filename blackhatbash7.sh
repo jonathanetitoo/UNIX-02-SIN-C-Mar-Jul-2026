82d967ce44d66c8270ecd663b02f0d8fe0ee0257
josev2609 <jovelascoso@uide.edu.ec>
2026-06-19 15:20:34 +0000

    awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -7,3 +7,7 @@ touch example_file1 example_file2 example_file3
   $ ps | grep -i tty
   $ grep -v "35.237.4.214" log.txt
    grep -o "35.237.4.214" log.txt
+   $ awk '{print $1}' log.txt
+   $ awk '{print $3}' log.txt
+   awk '{print $1,$2,$3}' log.txt
+   awk '{print $1,$NF}' log.txt
