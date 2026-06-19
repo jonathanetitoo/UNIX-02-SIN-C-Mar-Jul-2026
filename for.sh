6e54791188198e32c4ef8fc16bdf4c2a84a47dc5
josev2609 <jovelascoso@uide.edu.ec>
2026-06-19 14:39:20 +0000

    ip

--- for.sh
+++ for.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 for index in $(seq 1 10); do  
-    echo "${index}" 
+echo"${index}" 
 done
---
+++ for_ip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ for ip_address in "$@"; do 
+ echo "Taking some action on IP address ${8.8.8}" 
+ done
